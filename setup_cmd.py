ff7e766ea8302a5e7afabfda46de3c35b1807b28
Henrique <henrique.val@hotmail.com>
2022-04-02 19:24:53 -0300

    add which

--- setup_cmd.py
+++ setup_cmd.py
@@ -1,11 +1,20 @@
 import os
 
+def main():
+    program_path = os.getcwd()
+    home_path = os.path.expanduser('~')
 
-program_path = os.getcwd()
-home_path = os.path.expanduser('~')
+    with open(f'{home_path}\\.macros.doskey', 'a') as file:
+        file.write(
+            f'''ls=python {program_path}\\sources\\ls.py $1 $2\n
+            ll=python {program_path}\\sources\\ls.py -lac\n
+            cat=type $1\n
+            pwd=cd\n
+            mv=move $1 $2\n
+            rm=del $*\n''')
+        
+        
+    os.system(f'reg add "HKCU\\Software\\Microsoft\\Command Processor" /v Autorun /d "doskey /macrofile=\\"{home_path}\\.macros.doskey\"" /f')
 
-with open(f'{home_path}\\.macros.doskey', 'a') as file:
-    file.write(f'ls=python {program_path}\\sources\\ls.py $1 $2\n')
-    file.write(f'll=python {program_path}\\sources\\ls.py -lac')
-
-os.system(f'reg add "HKCU\\Software\\Microsoft\\Command Processor" /v Autorun /d "doskey /macrofile=\\"{home_path}\\.macros.doskey\"" /f')
+if __name__ == '__main__':
+    main()
